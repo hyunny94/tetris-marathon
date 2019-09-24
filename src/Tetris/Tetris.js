f0ce0d70cc68c93b5fce6b6088270d0e2aeb98d0
Hyun Kyo Jung <hj283@cornell.edu>
2019-09-24 13:32:21 +0900

    mode: cors added

--- src/Tetris/Tetris.js
+++ src/Tetris/Tetris.js
@@ -75,7 +75,7 @@ class Tetris extends React.Component {
         // fetch(process.env.REACT_APP_SERVER_HOST + '/api/v1/ranks', {
         fetch('https://kyotris.com' + '/api/v1/ranks', {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            // mode: 'cors', // no-cors, cors, *same-origin
+            mode: 'cors', // no-cors, cors, *same-origin
             // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             // credentials: 'same-origin', // include, *same-origin, omit
             headers: {
